27f79e704e9e4ee852f33ebd5dcd461e063df818
ahmedelfakirben2 <ahmed@hamidi23.onmicrosoft.com>
2025-02-15 17:55:38 +0100

    DB.js

--- config/db.js
+++ config/db.js
@@ -3,23 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'postgres',
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     define: {
-      timestamps: false
+        timestamps: false
     },
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-  }
-);
+});
 
 export default db;
